5fc6a87b7b4869c0b16e435feba967e9c7efc04b
pedro-ruela <pedro.ruela2010@gmail.com>
2026-04-27 18:48:24 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,14 +3,14 @@ export const config = {
   repoName: 'caderneta-2026',
 
   ownerName: 'Pedro Ruela',
-  ownerEmail: 'o_teu_email',
-  ownerLinkedIn: 'o_teu_linkedin',
+  ownerEmail: 'pedro.ruela2010@gmail.com',
+  ownerLinkedIn: 'www.linkedin.com/in/pedro-ruela-531045292',
 
   ownerLocation: 'Lisbon, Portugal',
   ownerLocationFlag: '🇵🇹',
 
-  googleSheetId: 'XXXX',
-  googleSheetGid: 'XXXX',
+  googleSheetId: '1RciV61XsvqRTXu34ViM7MEtlKP1p4Qhk7RQ7O0QxUo4',
+  googleSheetGid: '2000304351',
 
   defaultPricePerPack: 1.50,
   defaultStickersPerPack: 7,
